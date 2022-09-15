2fdcc3ff273094d9f943783a8ab1102687e69694
Yangtze <along008@gmail.com>
2022-09-15 10:22:55 -0700

    update graphql_base to 15

--- graphql_base/__manifest__.py
+++ graphql_base/__manifest__.py
@@ -5,14 +5,14 @@
     "name": "Graphql Base",
     "summary": """
         Base GraphQL/GraphiQL controller""",
-    "version": "14.0.1.0.0",
+    "version": "15.0.0.0.0",
     "license": "LGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/rest-framework",
     "depends": ["base"],
     "data": ["views/graphiql.xml"],
-    "external_dependencies": {"python": ["graphene", "graphql_server"]},
-    "development_status": "Production/Stable",
+    "external_dependencies": {"python": ["graphene", "graphql-server-core"]},
+    "development_status": "Beta",
     "maintainers": ["sbidoul"],
     "installable": False,
 }
